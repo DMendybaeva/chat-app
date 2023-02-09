3a34ba3353a87798996a802d069deca9324a459c
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-09 21:30:32 +0600

    feat: add login form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,30 @@
-const LoginPage = () => <div>Login</div>;
+import { useFormik } from 'formik';
+
+const LoginPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="name">
+        name
+        <input id="name" name="name" type="text" onChange={formik.handleChange} value={formik.values.name} />
+      </label>
+      <label htmlFor="password">
+        password
+        <input id="password" name="password" onChange={formik.handleChange} value={formik.values.lastName} />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default LoginPage;
