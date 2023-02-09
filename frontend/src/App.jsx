f34e600fe9252a7b04995497360402d5813f586d
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-09 20:40:52 +0600

    feat: configure routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello!</p>
-      </header>
-    </div>
-  );
-}
+import { Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const App = () => (
+  <div className="App">
+    <Link to="/">Home</Link>
+    <Link to="/login">Login</Link>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </div>
+);
 
 export default App;
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => (
+  <div>
+    This page doesn&#39;t exist. Go <Link to="/">Home</Link>
+  </div>
+);
+
+export default NotFoundPage;
