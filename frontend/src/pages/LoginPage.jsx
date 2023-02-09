5014060deacfe5ca13ced63db0bad03bb5f976f9
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-09 22:06:11 +0600

    feat: add yup

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const LoginPage = () => {
   const formik = useFormik({
@@ -6,6 +7,10 @@ const LoginPage = () => {
       name: '',
       password: '',
     },
+    validationSchema: Yup.object({
+      name: Yup.string().max(5, 'Must be 5 characters or less').required('Required'),
+      password: Yup.string().required('Required'),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values));
     },
@@ -16,10 +21,12 @@ const LoginPage = () => {
       <label htmlFor="name">
         name
         <input id="name" name="name" type="text" onChange={formik.handleChange} value={formik.values.name} />
+        <div>{formik.errors.name}</div>
       </label>
       <label htmlFor="password">
         password
         <input id="password" name="password" onChange={formik.handleChange} value={formik.values.lastName} />
+        <div>{formik.errors.password}</div>
       </label>
 
       <button type="submit">Submit</button>
