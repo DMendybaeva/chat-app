ce47a913b3b7485aec7e4022a0052aa50c2206dd
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-20 15:58:21 +0600

    fix: Channels, Messages components html

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -3,11 +3,16 @@ import { useSelector } from 'react-redux';
 const Channels = () => {
   const { channels } = useSelector((state) => state.channelsReducer);
   return (
-    <div className="channels">
+    <ul className="nav flex-column nav-pills nav-fill px-2">
       {channels.map((channel) => (
-        <div key={channel.id}>{channel.name}</div>
+        <li className="nav-item w-100" key={channel.id}>
+          <button type="button" className="w-100 rounded-0 text-start btn btn-secondary">
+            <span className="me-1">#</span>
+            {channel.name}
+          </button>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 export default Channels;
--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,9 +3,11 @@ import { useSelector } from 'react-redux';
 const Messages = () => {
   const { messages } = useSelector((state) => state.messagesReducer);
   return (
-    <div className="channels">
+    <div id="messages-box" className="chat-messages overflow-auto px-5 ">
       {messages.map((message) => (
-        <div key={message.id}>{message.text}</div>
+        <div className="text-break mb-2" key={message.id}>
+          <b>{message.id}</b>: {message.text}
+        </div>
       ))}
     </div>
   );
