a3997951e555b2a59f48fbbe6b080920db78ad15
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-21 12:52:25 +0600

    feat: class btn-secondary on channel

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentChannelId } from '../slices/channelsSlice';
 
@@ -7,12 +8,19 @@ const Channels = () => {
   const handleClick = (channel) => () => {
     dispatch(setCurrentChannelId(channel.id));
   };
+  const { currentChannelId } = useSelector((state) => state.channels);
 
   return (
     <ul className="nav flex-column nav-pills nav-fill px-2">
       {channels.map((channel) => (
         <li className="nav-item w-100" key={channel.id}>
-          <button type="button" className="w-100 rounded-0 text-start btn btn-secondary" onClick={handleClick(channel)}>
+          <button
+            type="button"
+            className={cn('w-100', 'rounded-0', 'text-start', 'btn', {
+              'btn-secondary': currentChannelId === channel.id,
+            })}
+            onClick={handleClick(channel)}
+          >
             <span className="me-1">#</span>
             {channel.name}
           </button>
