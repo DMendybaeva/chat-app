85cce2b9c128b277af26e897f7312af62903c741
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-02-21 16:13:21 +0600

    fix: useAuth check Provider

--- frontend/src/providers/AuthProvider/useAuth.js
+++ frontend/src/providers/AuthProvider/useAuth.js
@@ -2,4 +2,12 @@ import { useContext } from 'react';
 
 import { AuthContext } from './AuthContext';
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const value = useContext(AuthContext);
+
+  if (!value) {
+    throw new Error('useAuth should be used within AuthProvider');
+  }
+
+  return value;
+};
