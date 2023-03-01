a091e761e15d4f606bf20c37b669ade8ade89ed3
Diana Mendybaeva <DRMendybaeva@gmail.com>
2023-03-02 00:05:55 +0600

    feat: add auth logic for signup page

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,36 +1,101 @@
 import { Form, FloatingLabel, Button } from 'react-bootstrap';
-// import { useFormik } from 'formik';
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 import imgSrc from '../assets/signup.jpg';
+import { getNewUserValidation } from '../validation/getNewUserValidation';
+import { useAuth } from '../providers/AuthProvider/index';
 
-const SignupPage = () => (
-  <div className="container-fluid h-100">
-    <div className="row justify-content-center align-content-center h-100">
-      <div className="col-12 col-md-8 col-xxl-6">
-        <div className="card shadow-sm">
-          <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-            <img src={imgSrc} className="rounded-circle" alt="Регистрация" />
-            <Form className="w-50">
-              <h1 className="text-center mb-4">Регистрация</h1>
-              <Form.Group className="form-floating mb-3">
-                <FloatingLabel controlId="floatingUsername" label="Имя пользователя" className="mb-3">
-                  <Form.Control type="text" placeholder="username" />
-                </FloatingLabel>
-                <FloatingLabel controlId="floatingPassword" label="Пароль" className="mb-3">
-                  <Form.Control type="password" placeholder="password" />
-                </FloatingLabel>
-                <FloatingLabel controlId="floatingInput" label="Подтвердите пароль" className="mb-3">
-                  <Form.Control type="password" placeholder="password" />
-                </FloatingLabel>
-              </Form.Group>
-              <Button variant="outline-primary" type="submit" className="w-100">
-                Зарегистрироваться
-              </Button>
-            </Form>
+const SignupPage = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      repeatedPassword: '',
+    },
+    validationSchema: getNewUserValidation(),
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post('/api/v1/signup', { username: values.username, password: values.password });
+        localStorage.setItem('user', JSON.stringify(response.data));
+        auth.logIn();
+        navigate('/');
+      } catch (e) {
+        setError('Такой пользователь уже существует');
+      }
+    },
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+              <img src={imgSrc} className="rounded-circle" alt="Регистрация" />
+              <Form className="w-50" onSubmit={formik.handleSubmit}>
+                <h1 className="text-center mb-4">Регистрация</h1>
+                <Form.Group className="form-floating mb-3">
+                  <FloatingLabel label="Имя пользователя" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      id="username"
+                      name="username"
+                      placeholder="username"
+                      onChange={formik.handleChange}
+                      value={formik.values.username}
+                      isInvalid={formik.errors.username}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>
+                      {formik.errors.username}
+                    </Form.Control.Feedback>
+                  </FloatingLabel>
+                  <FloatingLabel label="Пароль" className="mb-3">
+                    <Form.Control
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="password"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                      isInvalid={formik.errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>
+                      {formik.errors.password}
+                    </Form.Control.Feedback>
+                  </FloatingLabel>
+                  <FloatingLabel label="Подтвердите пароль" className="mb-3">
+                    <Form.Control
+                      type="password"
+                      id="repeatedPassword"
+                      name="repeatedPassword"
+                      placeholder="password"
+                      onChange={formik.handleChange}
+                      value={formik.values.repeatedPassword}
+                      isInvalid={formik.errors.repeatedPassword}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>
+                      {formik.errors.repeatedPassword}
+                    </Form.Control.Feedback>
+                    {error && <div style={{ color: 'red' }}>{error}</div>}
+                  </FloatingLabel>
+                </Form.Group>
+                <Button variant="outline-primary" type="submit" className="w-100">
+                  Зарегистрироваться
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default SignupPage;
--- frontend/src/validation/getChannelValidationSchema.js
+++ frontend/src/validation/getChannelValidationSchema.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const AddChannelSchema = (channels) =>
+export const getChannelValidationSchema = (channels) =>
   yup.object({
     channelName: yup
       .string()
